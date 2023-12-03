0f62866eaf0d20523e07fe2fbac9b2d3315b91ec
AzunyY <59543274+AzunyY@users.noreply.github.com>
2023-12-03 17:59:32 +0000

    update app.py

--- cloud-Infrastructure/data/app-data/app.py
+++ cloud-Infrastructure/data/app-data/app.py
@@ -1,25 +1,16 @@
-import paho.mqtt.client as mqtt
+from flask import Flask, request, jsonify
 import mysql.connector
-import json
 
-average_velocities = {
-    "20 to 29": {"Male": 1.36, "Female": 1.34},
-    "30 to 39": {"Male": 1.43, "Female": 1.34},
-    "40 to 49": {"Male": 1.43, "Female": 1.39},
-    "50 to 59": {"Male": 1.43, "Female": 1.31},
-    "60 to 69": {"Male": 1.34, "Female": 1.24},
-    "70 to 79": {"Male": 1.26, "Female": 1.13},
-    "80 to 89": {"Male": 0.97, "Female": 0.94}
-}
+app = Flask(__name__)
 
-# Configurações de conexão com o banco de dados
 db_config = {
-    'host': 'db',  # Nome do serviço MySQL conforme definido no docker-compose.yml
+    'host': 'db',  
     'user': 'admin',
     'password': 'root',
     'database': 'dbIOT'
 }
 
+@app.route('/insert', methods=['POST'])
 def insert_data_into_db(data):
     try:
         connection = mysql.connector.connect(**db_config)
@@ -35,25 +26,107 @@ def insert_data_into_db(data):
     except mysql.connector.Error as err:
         print("Erro ao inserir dados na base de dados:", err)
 
-def on_connect(client, userdata, flags, return_code):
-    if return_code == 0:
-        print("Conectado ao MQTT Broker")
-        client.subscribe("idc/fitness")
-    else:
-        print("Não foi possível conectar, código de retorno:", return_code)
+@app.route('/activities', methods=['GET'])
+def get_activities():
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
 
-def on_message(client, userdata, message):
-    received_data = str(message.payload.decode("utf-8"))
-    print("Dados recebidos:", received_data)
-    data_dict = json.loads(received_data)
-    insert_data_into_db(data_dict)
+        query = "SELECT * FROM Activities"
+        cursor.execute(query)
+        data = cursor.fetchall()
 
-broker_hostname = "localhost"
-port = 1883
+        cursor.close()
+        connection.close()
 
-client = mqtt.Client("Client1")
-client.on_connect = on_connect
-client.on_message = on_message
+        activities_list = []
+        for row in data:
+            activity = {
+                'ActivityID': row[0],
+                'Date': row[1].strftime("%Y-%m-%d"),
+                'ActivityType': row[2],
+                'Distance': float(row[3]),
+                'Duration': str(row[4]),
+                'CaloriesBurned': float(row[5])
+            }
+            activities_list.append(activity)
 
-client.connect(broker_hostname, port)
-client.loop_forever()
+        return jsonify(activities_list), 200
+    except mysql.connector.Error as err:
+        return jsonify({'error': str(err)}), 500
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM Users WHERE UserID = %s"
+        cursor.execute(query, (user_id,))
+        data = cursor.fetchone()
+
+        cursor.close()
+        connection.close()
+
+        if data:
+            user = {
+                'Age': data[2],
+                'Gender': data[3],
+                'Weight': float(data[4]),
+                'Height': float(data[5]),
+                'Goal': int(data[6])
+            }
+            return jsonify(user), 200
+        else:
+            return jsonify({'error': 'User not found'}), 404
+
+    except mysql.connector.Error as err:
+        return jsonify({'error': str(err)}), 500
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    try:
+        connection =  mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+
+        user_data = request.get_json()
+        query = "INSERT INTO Users (UserName, Age, Gender, Weight, Height, Goal) VALUES (%s, %s, %s, %s, %s, %s)"
+        cursor.execute(query, (user_data['UserName'], user_data['Age'], user_data['Gender'],
+                               user_data['Weight'], user_data['Height'], user_data['Goal']))
+        
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        return jsonify({'message': 'User created successfully'}), 201
+    except mysql.connector.Error as err:
+        return jsonify({'error': str(err)}), 500
+
+@app.route('/velocity', methods=['GET'])
+def get_velocity():
+    try:
+        age = int(request.args.get('age'))
+        gender = request.args.get('gender')
+        gender_value = 1 if gender.lower() == 'female' else 0
+
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+
+        query = "SELECT Velocity FROM Velocities WHERE Range_start <= %s AND Range_finished >= %s AND Genre = %s"
+        cursor.execute(query, (age, age, gender_value))
+        velocity = cursor.fetchone()
+
+        cursor.close()
+        connection.close()
+
+        if velocity:
+            return jsonify({'velocity': float(velocity[0])}), 200
+        else:
+            return jsonify({'error': 'Velocity not found for the given parameters'}), 404
+
+    except mysql.connector.Error as err:
+        return jsonify({'error': str(err)}), 500
+    
+if __name__ == '__main__':
+    app.run(debug=True)
